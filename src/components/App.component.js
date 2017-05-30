ad80019996a69202215370f67408a803c1d05cb8
Arpit Goyal <arpit.go4@gmail.com>
2017-05-30 17:45:30 +0530

    project scaffold

---
+++ src/components/App.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class App extends React.Component {
+
+	render() {
+		return (
+			<div className="container-fluid">
+				<h1>Bootstrap Exercise 1</h1>
+			</div>
+		);
+	}
+
+}
