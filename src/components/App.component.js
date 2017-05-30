f3d24ab3714717595f51564d97f23ef8b5c7ddf7
Arpit Goyal <arpit.go4@gmail.com>
2017-05-30 18:53:48 +0530

    all tiles added

--- src/components/App.component.js
+++ src/components/App.component.js
@@ -5,7 +5,38 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<div className="container-fluid">
-				<h1>Bootstrap Exercise 1</h1>
+				<div className="row">
+					<div className="header col-xs-12"></div>
+				</div>
+				<div className="row">
+					<div className="col-md-2 navbar"></div>
+					<div className="col-md-10">
+						<div className="row body">
+							<div className="title col-xs-12"></div>
+						</div>
+						<div className="row">
+							<div className="col-md-5 big-tile"></div>
+							<div className="col-md-offset-1 col-md-5 big-tile"></div>
+						</div>
+						<div className="row">
+							<div className="col-md-2 small-tile"></div>
+							<div className="col-md-2 small-tile"></div>
+							<div className="col-md-3 small-tile"></div>
+							<div className="col-md-2 small-tile"></div>
+							<div className="col-md-2 small-tile"></div>
+						</div>
+						<div className="row">
+							<div className="col-md-2 small-tile"></div>
+							<div className="col-md-2 small-tile"></div>
+							<div className="col-md-3 small-tile"></div>
+							<div className="col-md-2 small-tile"></div>
+							<div className="col-md-2 small-tile"></div>
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-12 footer" />
+				</div>
 			</div>
 		);
 	}
