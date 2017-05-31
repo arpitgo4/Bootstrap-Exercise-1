89950a877244ed4580e2fb4b8ad8e9ac062f118a
Arpit Goyal <arpit.go4@gmail.com>
2017-05-31 16:17:16 +0530

    layout rows and cols adjuested

--- src/components/App.component.js
+++ src/components/App.component.js
@@ -10,28 +10,30 @@ export default class App extends React.Component {
 				</div>
 				<div className="row">
 					<div className="col-md-1 col-sm-1 hidden-xs navbar"></div>
-					<div className="col-md-10 col-sm-10 body">
-						<div className="row">
-							<div className="col-xs-12 title-col"><div className="title" /></div>
-						</div>
-						<div className="row big-tile-container">
-							<div className="col-md-6 col-sm-12 big-tile-col"><div className="big-tile" /></div>
-							<div className=" col-md-6 col-sm-12 big-tile-col"><div className="big-tile" /></div>
-						</div>
-						<div className="small-tile-container">
-							<div className="row small-tile-row">
-								<div className="col-md-2 small-tile"></div>
-								<div className="col-md-2 small-tile"></div>
-								<div className="col-md-3 small-tile"></div>
-								<div className="col-md-2 small-tile"></div>
-								<div className="col-md-2 small-tile"></div>
+					<div className="col-md-11 col-sm-11 col-xs-12 body-col">
+						<div className="body">
+							<div className="row">
+								<div className="col-xs-12 title-col"><div className="title" /></div>
 							</div>
-							<div className="row small-tile-row">
-								<div className="col-md-2 small-tile"></div>
-								<div className="col-md-2 small-tile"></div>
-								<div className="col-md-3 small-tile"></div>
-								<div className="col-md-2 small-tile"></div>
-								<div className="col-md-2 small-tile"></div>
+							<div className="row big-tile-container">
+								<div className="col-md-6 col-sm-12 big-tile-col"><div className="big-tile" /></div>
+								<div className=" col-md-6 col-sm-12 big-tile-col"><div className="big-tile" /></div>
+							</div>
+							<div className="small-tile-container">
+								<div className="row small-tile-row">
+									<div className="col-md-2"><div className="small-tile" /></div>
+									<div className="col-md-2"><div className="small-tile" /></div>
+									<div className="col-md-3"><div className="small-tile" /></div>
+									<div className="col-md-2"><div className="small-tile" /></div>
+									<div className="col-md-2"><div className="small-tile" /></div>
+								</div>
+								<div className="row small-tile-row">
+									<div className="col-md-2"><div className="small-tile" /></div>
+									<div className="col-md-2"><div className="small-tile" /></div>
+									<div className="col-md-3"><div className="small-tile" /></div>
+									<div className="col-md-2"><div className="small-tile" /></div>
+									<div className="col-md-2"><div className="small-tile" /></div>
+								</div>
 							</div>
 						</div>
 					</div>
