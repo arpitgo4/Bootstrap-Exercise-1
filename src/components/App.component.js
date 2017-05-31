e27e9b1755d74e3cf0a1bee9e4f616b379f5ad40
Arpit Goyal <arpit.go4@gmail.com>
2017-05-31 13:04:17 +0530

    big tile adjusted for small screens

--- src/components/App.component.js
+++ src/components/App.component.js
@@ -12,11 +12,11 @@ export default class App extends React.Component {
 					<div className="col-md-1 col-sm-1 hidden-xs navbar"></div>
 					<div className="col-md-10 col-sm-10 body">
 						<div className="row">
-							<div className="title col-xs-12"></div>
+							<div className="col-xs-12 title-col"><div className="title" /></div>
 						</div>
 						<div className="row big-tile-container">
-							<div className="col-md-5 col-sm-5 big-tile"></div>
-							<div className=" col-md-5 col-sm-5 big-tile"></div>
+							<div className="col-md-6 col-sm-12 big-tile-col"><div className="big-tile" /></div>
+							<div className=" col-md-6 col-sm-12 big-tile-col"><div className="big-tile" /></div>
 						</div>
 						<div className="small-tile-container">
 							<div className="row small-tile-row">
